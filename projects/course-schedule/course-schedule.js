3301f571ff17741416b5268cdea29b491c4d4505
johnlaschober <johnlaschober@gmail.com>
2018-09-26 13:43:05 -0500

    Added map tutorial site

--- projects/course-schedule/course-schedule.js
+++ projects/course-schedule/course-schedule.js
@@ -1,3 +1,8 @@
+// Author: John Laschober
+// Course Name: Web and Distributed Programming
+// Project Number: 2
+// Semester: Fall 2018
+
 function infoSubmit() {
     var name = document.getElementById("enteredName").value;
     window.alert("Hi " + name + "! Let's pretend this information was sent somewhere!");
