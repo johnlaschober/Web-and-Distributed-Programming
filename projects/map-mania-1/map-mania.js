7f67b6978c1cb2af5ab6dec8efb1a828af7b97ec
johnlaschober <johnlaschober@gmail.com>
2018-09-28 16:21:58 -0500

    separated map-mania-1 files

---
+++ projects/map-mania-1/map-mania.js
@@ -0,0 +1,44 @@
+var map;
+
+function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 20, lng: -80},
+            zoom: 8
+            // mapTypeId: 'satellite'
+    });
+
+    var myLatLng = new google.maps.LatLng({lat: -23, lng: 131}); 
+        
+
+    var testMarker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'This is a test marker at a test location.'
+    });
+
+    testMarker.addListener('click', function() {
+        map.setZoom(8);
+        map.setCenter(testMarker.getPosition());
+    }); 
+
+    map.addListener('idle', function() {
+        // 0=zoomed out | 22=zoomed in
+        document.getElementById("zoomLevel").innerHTML = "Zoom level: " + map.zoom;
+
+        var bounds = map.getBounds();
+
+        if (bounds.contains(myLatLng))
+        {
+            document.getElementById("inBoundStatus").innerHTML = "myLatLng object is in the current bounds." ;
+
+            if (map.getZoom() >= 14)
+            {
+                document.getElementById("inBoundStatus").innerHTML = "myLatLng object is in the current bounds and at the minimum zoom level (14).";
+            }
+        }
+        else
+        {
+        document.getElementById("inBoundStatus").innerHTML = "myLatLng object is NOT in the current bounds.";
+        }
+    });
+}
