6b1a00c73fbf4d3acbfbf60dea0bafc938038cfd
John Laschober <johnlaschober@gmail.com>
2018-10-04 17:22:07 -0500

    Finished dice roller

--- projects/dice-roller/dice-roller.js
+++ projects/dice-roller/dice-roller.js
@@ -1,5 +1,7 @@
 function RollSixSidedDie() {
-    document.getElementById("die6").value = Math.floor(Math.random()*6)+1;
+    var randomValue = Math.floor(Math.random()*6)+1;
+    document.getElementById("die6").value = randomValue;
+    document.getElementById("die6ImageText").innerHTML = randomValue;
 
     var dieImage = document.getElementById("die6Image");
     dieImage.style.animation = 'jump 0.5s linear';
@@ -9,7 +11,15 @@ function RollSixSidedDie() {
 }
 
 function RollEightSidedDie() {
-    document.getElementById("die8").value = Math.floor(Math.random()*8)+1;
+    var randomValue = Math.floor(Math.random()*8)+1;
+    document.getElementById("die8").value = randomValue;
+    document.getElementById("die8ImageText").innerHTML = randomValue;
+
+    var dieImage = document.getElementById("die8Image");
+    dieImage.style.animation = 'jump 0.5s linear';
+    setTimeout(function() {
+        dieImage.style.animation = 'none';
+    }, 500);
 }
 
 function initApp() {
