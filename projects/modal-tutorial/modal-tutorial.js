305362f6d9fb46691e52cf9d9f874f18d48deb0f
johnlaschober <johnlaschober@gmail.com>
2018-10-17 15:44:10 -0500

    modal-tutorial

---
+++ projects/modal-tutorial/modal-tutorial.js
@@ -0,0 +1,13 @@
+function initApp() {
+    console.log("Working");
+}
+
+function openHelp() {
+    var help = document.getElementById('helpID');
+    help.style.display = "block";
+}
+
+function closeHelp() {
+    var help = document.getElementById('helpID');
+    help.style.display = "none";
+}
